de598efa158eb698acfcbb88eefe0182990e3da0
darco <1014991393@qq.com>
2022-10-25 08:35:51 +0800

    get figure for paper

--- SUMOFiles/edgesDensity/pltEdgesDensity.py
+++ SUMOFiles/edgesDensity/pltEdgesDensity.py
@@ -44,17 +44,17 @@ def writeFile():
     randomAvgDen = []
     for rk, rv in randomEMD.items():
         if rk in intersectionKeys:
-            randomAvgDen.append(np.average(rv))
+            randomAvgDen.append(np.max(rv))
 
     ARNMAvgDen = []
     for tk, tv in ARNMEMD.items():
         if tk in intersectionKeys:
-            ARNMAvgDen.append(np.average(tv))
+            ARNMAvgDen.append(np.max(tv))
 
     PPPAvgDen = []
     for pk, pv in PPPEMD.items():
         if pk in intersectionKeys:
-            PPPAvgDen.append(np.average(pv))
+            PPPAvgDen.append(np.max(pv))
 
     randomAvgDen.sort()
     ARNMAvgDen.sort()
@@ -76,11 +76,13 @@ def pltshow(npRandomEMD, npARNMEMD, npPPPEMD):
     diff_1 = npRandomEMD - npARNMEMD
     diff_2 = npPPPEMD - npARNMEMD
     xx = list(range(len(npRandomEMD)))
-    plt.figure(figsize=(9, 10))
+    plt.figure(figsize=(8, 9.2))
     plt.subplot(2, 1, 1)
     plt.plot(xx, npRandomEMD)
     plt.plot(xx, npPPPEMD, color='gray')
     plt.plot(xx, npARNMEMD)
+
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
 
     # plt.fill_between(xx, npRandomEMD, npARNMEMD, alpha=0.5)
     plt.ylabel('Density (veh/km)')
@@ -91,15 +93,17 @@ def pltshow(npRandomEMD, npARNMEMD, npPPPEMD):
     plt.plot(xx, diff_1, color='#45aaf2')
     shadow1 = plt.fill_between(xx, zeroLine, diff_1, color='#45aaf2', alpha=0.5)
     plt.plot(xx, diff_2, color='#e67e22')
-    shadow2 = plt.fill_between(xx, zeroLine, diff_2, color='#e67e22', alpha=0.8)
+    shadow2 = plt.fill_between(xx, zeroLine, diff_2, color='#e67e22', alpha=0.5)
     plt.legend([shadow1, shadow2], ['DD between duarouter and ARNM', 'DD between PPP and ARNM'])
     plt.xlabel('Edges')
     plt.ylabel('Density difference (veh/km)')
-    plt.savefig('DensityDiff.png')
+    plt.subplots_adjust(hspace=0.05)
+
+    plt.savefig('DensityDiff.png', bbox_inches='tight', pad_inches=0.2)
 
 
 if __name__ == '__main__':
     # writeFile()
     npRandomEMD, npARNMEMD, npPPPEMD = readFile()
     # print(len(npRandomEMD))
-    pltshow(npRandomEMD[-500:], npARNMEMD[-500:], npPPPEMD[-500:])
+    pltshow(npRandomEMD[-1000:], npARNMEMD[-1000:], npPPPEMD[-1000:])
